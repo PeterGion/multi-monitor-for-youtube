402d2bd7585a61939df22b5889aa6a835f80c564
PeterGion <Peter.j.Gionfriddo@gmail.com>
2022-05-27 19:08:14 -0400

    created the home page test code and working on vidoes

--- redo/script.js
+++ redo/script.js
@@ -1,5 +1,5 @@
 
-const intial = function(){
+ function homepageIntial(){
     let gridRow = document.querySelectorAll('ytd-rich-grid-row > div > ytd-rich-item-renderer');
     //let gridRows = document.querySelectorAll('ytd-rich-grid-row');
     //console.log(gridRows.length);
@@ -7,25 +7,32 @@ const intial = function(){
     //console.log(gridRow.length);
     for(let i = 0; i < gridRow.length; i++)
     {
-        let divInjection = document.createElement('div');
-        divInjection.className = 'test';
-        divInjection.innerHTML = 'this is a test';
-        gridRow[i].append(divInjection);
+        if(gridRow[i].querySelector('.test') === null)
+        {
+            let divInjection = createInjectionElement();
+            gridRow[i].append(divInjection);
+        }
     }
     //console.log(gridRow[0]);
     //gridRow.append(divInjection);
 }
 document.addEventListener("yt-navigate-finish", function(event) { 
-    isVideoLoaded();
-    console.log(window.location.href);
-    isVideo();
+    //observer.disconnect();
+    if (jsInitChecktimer !== null) 
+        clearInterval(jsInitChecktimer);
+    setState();
 });
+
 function isVideo()
 {
-    let url = new URL(window.location.href)
-    videoId = url.searchParams.get("v")
-    console.log(document.querySelector(`ytd-watch-flexy[video-id='${videoId}']`) !== null)
-    return document.querySelector(`ytd-watch-flexy[video-id='${videoId}']`) !== null
+    let url = new URL(window.location.href);
+    videoId = url.searchParams.get("v");
+    //console.log(document.querySelector(`ytd-watch-flexy[video-id='${videoId}']`));
+    return(
+        document.querySelector(`ytd-watch-flexy[video-id='${videoId}']`) !== null ||
+        // mobile: no video-id attribute
+        document.querySelector('#player[loading="false"]:not([hidden])') !== null
+    );
 }
 //console.log(document.querySelector('#contents'));
 //let gridRows = document.querySelector('ytd-rich-grid-row');
@@ -34,7 +41,7 @@ let gridRows = document.querySelector('#contents');
 // Options for the observer (which mutations to observe)
 const config = {childList: true};
 
-const callback = function(mutationList, observer) {
+const homeCallback = function(mutationList, observer) {
     for(const mutation of mutationList) {
         let gridRow = document.querySelectorAll('ytd-rich-grid-row > div > ytd-rich-item-renderer');
         if (mutation.type === 'childList') {
@@ -45,17 +52,76 @@ const callback = function(mutationList, observer) {
                 //only add the button if one isn't already present
                 if(gridRow[i].querySelector('.test') === null)
                 {
-                    let divInjection = document.createElement('div');
-                    divInjection.className = 'test';
-                    divInjection.innerHTML = 'this is a test';
+                    let divInjection = createInjectionElement();
                     gridRow[i].append(divInjection);
                 }
             }
         }
     }
 };
-const observer = new MutationObserver(callback);
 
-observer.observe(gridRows, config);
+function createInjectionElement()
+{
+    let divInjection = document.createElement('div');
+    divInjection.className = 'test';
+    divInjection.innerHTML = 'this is a test';
+    return divInjection;
+}
 
-//intial();
+let jsInitChecktimer = null;
+//let sideBarVids = document.querySelector('#items.style-scope.ytd-watch-next-secondary-results-renderer');
+function setState()
+{
+    jsInitChecktimer = setInterval(checkForJS_Finish, 111);
+    function checkForJS_Finish()
+    {
+        {
+            let url = window.location.href;
+            if(url === 'https://www.youtube.com/')
+            {
+                console.log('this is the home page');
+                homepageIntial();
+                let observer = new MutationObserver(homeCallback);
+                observer.observe(gridRows, config);
+                //console.log(typeof gridRows);
+                clearInterval(jsInitChecktimer);
+                jsInitChecktimer = null;
+            }
+            else if(isVideo())
+            {
+                clearInterval(jsInitChecktimer);
+                jsInitChecktimer = null;
+                console.log('this is a video');
+                window.setTimeout(addObserverIfDesiredNodeAvailable,1000);
+                function addObserverIfDesiredNodeAvailable(){
+                    let sideBarVids = document.querySelector('#items.style-scope.ytd-watch-next-secondary-results-renderer>ytd-item-section-renderer>#contents');
+                    console.log(sideBarVids);
+                    //let test2 = test.querySelector(':nth-child(2)');
+                    let sidebar = new MutationObserver(videoCallBack);
+                    sidebar.observe(sideBarVids, config);
+                }
+            }
+        }
+    }
+}
+function videoCallBack(mutationList, observer)
+{
+    for(const mutation of mutationList) {
+        if (mutation.type === 'childList')
+        {
+            console.log('more children loaded');
+            document.querySelectorAll('ytd-compact-video-renderer')
+            let sideBarVidsAll = document.querySelectorAll('#items.style-scope.ytd-watch-next-secondary-results-renderer>ytd-item-section-renderer>#contents');
+            for(let i = 0; i < sideBarVidsAll.length; i++)
+            {
+                //infinate loop here
+                let divInjection = createInjectionElement();
+                sideBarVidsAll[i].append(divInjection);
+            }
+            
+            //sideBarVids = sideBarVids.querySelector('ytd-item-section-renderer:nth-child(1):nth-child(2)')
+        }   
+    }
+}
+
+setState();
